46806438ab5d479ad2c03fe8f33d5bd341d1df1e
Bethany Walker <reklaw.bethany@gmail.com>
2019-01-08 07:11:07 -0500

    Changed HashRouter to BrowserRouter

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import store from './redux/store';
 import Main from './components/main';
@@ -10,12 +10,12 @@ import BookDetails from './components/bookDetails';
 ReactDOM.render(
   <div>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <div>
           <Route exact path="/" component={Main} />
           <Route path="/book/:bookKey" component={BookDetails} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </div>,
   document.getElementById('app')
--- client/forNow.js
+++ client/forNow.js
@@ -1,3 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import store from './redux/store';
+import Main from './components/main';
+import BookDetails from './components/bookDetails';
+
+ReactDOM.render(
+  <div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Main} />
+          <Route path="/book/:bookKey" component={BookDetails} />
+        </div>
+      </BrowserRouter>
+    </Provider>
+  </div>,
+  document.getElementById('app')
+);
+
+
+
+
+=====================
+
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
