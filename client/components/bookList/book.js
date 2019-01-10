676c8888e3099ac07cad667ab3fef837297e6ee1
Bethany Walker <reklaw.bethany@gmail.com>
2019-01-09 22:06:58 -0500

    Added more CSS to books in list

--- client/components/bookList/book.js
+++ client/components/bookList/book.js
@@ -5,9 +5,14 @@ const Book = (props) => {
   const book = props.book;
   return (
     <div className="small-book">
-      <div>{BookImage(book, 'S')}</div>
-      <p>{book.title} ({book.first_publish_year || <i>n/a</i>})</p>
-      <p><i>{book.author_name ? book.author_name.join(', ') : 'unknown'}</i></p>
+      <h3 id="small-title">{book.title}</h3>
+      <div className="img-info-div">
+        {BookImage(book, 'S')}
+        <div>
+          <p><i>{book.author_name ? book.author_name.join(', ') : 'unknown'}</i></p>
+          <p>first published in: {book.first_publish_year || <i>unknown</i>}</p>
+        </div>
+      </div>
     </div>
   );
 };
